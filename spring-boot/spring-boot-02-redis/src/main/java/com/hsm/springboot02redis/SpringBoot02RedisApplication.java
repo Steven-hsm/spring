ff81520038922ea338db62e2a100026f7ba9bf53
黄森明 <1586558083@qq.com>
2019-12-28 17:35:52 +0800

    临时提交

---
+++ spring-boot/spring-boot-02-redis/src/main/java/com/hsm/springboot02redis/SpringBoot02RedisApplication.java
@@ -0,0 +1,13 @@
+package com.hsm.springboot02redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot02RedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot02RedisApplication.class, args);
+    }
+
+}
