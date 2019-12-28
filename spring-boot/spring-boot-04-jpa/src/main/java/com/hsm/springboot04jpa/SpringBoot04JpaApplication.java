22c378761a804013c46e1ad028f1f9ce04e8acc7
黄森明 <1586558083@qq.com>
2019-12-28 18:38:34 +0800

    临时提交

---
+++ spring-boot/spring-boot-04-jpa/src/main/java/com/hsm/springboot04jpa/SpringBoot04JpaApplication.java
@@ -0,0 +1,13 @@
+package com.hsm.springboot04jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot04JpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot04JpaApplication.class, args);
+    }
+
+}
